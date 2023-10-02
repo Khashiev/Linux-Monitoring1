fbdb857950ff7d3535345cae9a865b0bb02cb6e2
Khashiev <nxashiev@gmail.com>
2023-10-02 12:29:29 +0300

    add hash

--- src/05/main.sh
+++ src/05/main.sh
@@ -6,6 +6,8 @@ if [[ $# -ne 1 ]]; then
     echo "incorrect input"
 elif [ ! -d "$1" ]; then
     echo "No such file or directory"
+elif [[ ${1: -1} != "/" ]]; then
+    echo Directory path should end with '/'
 else
     . ./print.sh
 fi
--- src/05/vars.sh
+++ src/05/vars.sh
@@ -14,4 +14,4 @@ archive_files=$(find $1 -regex '.*\(tar\|zip\|gz\|rar\)' | wc -l)
 link_files=$(find $1 -type l | wc -l)
 
 top_size_files=$(find $1 -type f -exec du -h {} + | sort -rh | head -n 10 | awk -F"\t" '{printf "- %s, %s\n", $2, $1}' | nl -w1 -s" ")
-top_size_exe_files=$(find $1 -type f -executable -exec du -h {} + | sort -rh | head -n 10 | awk -F"\t" '{printf "- %s, %s\n", $2, $1}' | nl -w1 -s" ")
+top_size_exe_files=$(find $1* -type f -executable -exec du -h {} + 2>/dev/null | sort -hr | sed -n '1,10'p | awk '{printf("%d %s %s ",NR,$2, $1); system("bash -c '\''md5sum "$2"'\''")}' | awk '{printf("%d - %s, %s, %s\n",NR,$2, $3, $4 )}')
